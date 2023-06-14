7bf7748c74d2ab15ce37c4342433d029fe3f09fb
Lucas Sandin <lucasfernandessandin@gmail.com>
2023-06-14 13:17:40 -0300

    update

--- src/Pages/ReconhecimentoFacial/index.js
+++ src/Pages/ReconhecimentoFacial/index.js
@@ -69,7 +69,7 @@ export default function ReconhecimentoFacial() {
                 onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
                 onTakePhotoAnimationDone = { (dataUri) => { handleTakePhotoAnimationDone(dataUri); } }
                 onCameraError = { (error) => { handleCameraError(error); } }
-                idealFacingMode = {FACING_MODES.ENVIRONMENT}
+                idealFacingMode = {FACING_MODES.USER}
                 idealResolution = {{width: 640, height: 480}}
                 imageType = {IMAGE_TYPES.JPG}
                 imageCompression = {0.97}
