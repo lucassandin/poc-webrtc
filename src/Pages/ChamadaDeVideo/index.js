9c3adc486ea72f19044f967e9282dee2504c6274
Lucas Sandin <lucasfernandessandin@gmail.com>
2023-06-19 21:58:19 -0300

    update

--- src/Pages/ChamadaDeVideo/index.js
+++ src/Pages/ChamadaDeVideo/index.js
@@ -63,6 +63,8 @@ export default function ChamadaDeVideo() {
     // screenshare.current.session = session;
     // screenshare.current.token = token;
 
+    session.connect(token);
+
     return () => {
       session.disconnect();
     }
